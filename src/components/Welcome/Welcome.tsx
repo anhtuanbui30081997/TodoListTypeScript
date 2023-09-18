17a3acd5a23a16907e17d2bf0a1dcb354e3b202a
AnhTuanBui <buituananh30081997@gmail.com>
2023-09-18 23:04:26 +0700

    update useContext and apply useId

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext, useId, useMemo, useCallback } from 'react'
 import styles from './welcome.module.scss'
 
 interface ThemeType {
@@ -17,14 +17,28 @@ const ThemeContext = createContext<ThemeType>({
 
 export default function Welcome() {
   const [theme, setTheme] = useState<ThemeType['theme']>({ color: 'dark' })
-  const onChangeTheme = (color: 'light' | 'dark') => {
+  const [, forceRender] = useState({})
+
+  const onChangeTheme = useCallback((color: 'light' | 'dark') => {
     setTheme((prev) => ({ ...prev, color }))
-  }
+  }, [])
+
+  const pleaseRender = () => forceRender({})
+
+  // Note: chu y khong nen gan gia cho value cua useContext la mot object vi khi re render lai component thi se
+  // tao ra mot value moi nen no se gay ra re render o cac component con
+  // cach khac phuc la sd useMemo cho object khoi tao trong value
+
+  const valueContext = useMemo(() => {
+    return { theme, onChangeTheme }
+  }, [theme, onChangeTheme])
+
   return (
     <div className={styles.welcome}>
-      <ThemeContext.Provider value={{ theme, onChangeTheme }}>
+      <ThemeContext.Provider value={valueContext}>
         <Form />
         <Label />
+        <button onClick={pleaseRender}>Please Render Welcome</button>
       </ThemeContext.Provider>
     </div>
   )
@@ -54,11 +68,14 @@ function Button({ children }: { children: React.ReactNode }) {
   return <button className={theme.color === 'dark' ? styles.button_dark : styles.button_light}>{children}</button>
 }
 
-function Label() {
+const Label = React.memo(() => {
   const { theme, onChangeTheme } = useContext(ThemeContext)
+  const id = useId()
+  console.log('render')
   return (
-    <label htmlFor='theme'>
+    <label htmlFor={id}>
       <input
+        id={id}
         type='checkbox'
         checked={theme.color === 'dark'}
         onChange={(e) => onChangeTheme(e.target.checked ? 'dark' : 'light')}
@@ -66,4 +83,4 @@ function Label() {
       Use dark mode
     </label>
   )
-}
+})
